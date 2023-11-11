d22bec5be10d8ee1a41c27acea49719889f96e4f
David Szczepanik <davidszczepanik@mail.com>
2023-11-11 17:41:35 +0100

    1

--- The_Odin_Project/javascript.js
+++ The_Odin_Project/javascript.js
@@ -7,7 +7,6 @@ let message = 'Hello',
 //num = 5; num is not defined, compatible with old JS without "use strict"
 
 alert(message);
-
 const myBirthday = '3.12.1997'; //lower-case constant because it's not known prior to the page load
 alert(myBirthday);
 
@@ -17,14 +16,17 @@ const COLOR_BLUE = "#00F";
 const COLOR_ORANGE = "#FF7F00";
 
 let color = COLOR_ORANGE;
-alert(color); // #FF7F00
 
+//alert(color); // #FF7F00
 let name = 'John',
   admin = name;
 
-alert(admin);
+//alert(admin);
 
 const PLANET = 'Earth'; //ourPlanetName
 let currentUserName = "John"; //userName
 
-
+let x = +"100";
+let y = +"10";
+let z = x + y;
+console.log(z);
